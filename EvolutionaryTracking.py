6eca7006ac1c1e5a25153fec51b6d6a593d9027b
Anuraag Bukkuri <abukkuri@gmail.com>
2023-09-19 07:59:49 -0400

    Update EvolutionaryTracking.py

--- EvolutionaryTracking.py
+++ EvolutionaryTracking.py
@@ -1,15 +1,30 @@
+import numpy as np
+from scipy.integrate import *
+import matplotlib.pyplot as plt
+import math
+from math import exp
+
+pop1 = 10
+pop2 = 10
+
+KM = 100
+d = [0.05, 0.05]
+r = [0.25, 0.25]
+k = [.2, .5]
+B = 0
+sk = 12.5
 strat1 = strat2 = 0.5
 time = 1000
-sa=100
+sa = 100
 extinct_fast = 0
 extinct_slow = 0
 
-IC = [pop1,pop2,strat1,strat2]
+IC = [pop1, pop2, strat1, strat2]
 
-#Code for ODE simulation
+
+# Code for ODE simulation
 def evoLV(X, t):
-
-    gamma = np.sin(t/50)
+    gamma = np.sin(t / 50)
 
     x1 = X[0]
     x2 = X[1]
@@ -22,11 +37,21 @@ def evoLV(X, t):
     a2 = 1 + math.exp(-(u1 - u2 + B) ** 2 / (2 * sa)) - math.exp(-(B ** 2 / (2 * sa)))
     a1 = 1 + math.exp(-(u2 - u1 + B) ** 2 / (2 * sa)) - math.exp(-(B ** 2 / (2 * sa)))
 
-    dx1dt = x1 * (r[0]/K1 * (K1 - a2*x2 - x1) - d[0]*k[0])
-    dx2dt = x2 * (r[1]/K2 * (K2 - a1*x1 - x2) - d[1]*k[1])
+    dx1dt = x1 * (r[0] / K1 * (K1 - a2 * x2 - x1) - d[0] * k[0])
+    dx2dt = x2 * (r[1] / K2 * (K2 - a1 * x1 - x2) - d[1] * k[1])
 
-    dG1dv = r[0]*(-KM*(-2*gamma + 2*u1)*exp(-(-gamma + u1)**2/(2*sk))/(2*sk) + x2*(2*B + 2*u1 - 2*u2)*exp(-(B + u1 - u2)**2/(2*sa))/(2*sa))*exp((-gamma + u1)**2/(2*sk))/KM + r[0]*(-2*gamma + 2*u1)*(KM*exp(-(-gamma + u1)**2/(2*sk)) - x1 - x2*(1 + exp(-(B + u1 - u2)**2/(2*sa)) - exp(-B**2/(2*sa))))*exp((-gamma + u1)**2/(2*sk))/(2*KM*sk)
-    dG2dv = r[1]*(-KM*(-2*gamma + 2*u2)*exp(-(-gamma + u2)**2/(2*sk))/(2*sk) + x1*(2*B - 2*u1 + 2*u2)*exp(-(B - u1 + u2)**2/(2*sa))/(2*sa))*exp((-gamma + u2)**2/(2*sk))/KM + r[1]*(-2*gamma + 2*u2)*(KM*exp(-(-gamma + u2)**2/(2*sk)) - x1*(1 + exp(-(B - u1 + u2)**2/(2*sa)) - exp(-B**2/(2*sa))) - x2)*exp((-gamma + u2)**2/(2*sk))/(2*KM*sk)
+    dG1dv = r[0] * (-KM * (-2 * gamma + 2 * u1) * exp(-(-gamma + u1) ** 2 / (2 * sk)) / (2 * sk) + x2 * (
+                2 * B + 2 * u1 - 2 * u2) * exp(-(B + u1 - u2) ** 2 / (2 * sa)) / (2 * sa)) * exp(
+        (-gamma + u1) ** 2 / (2 * sk)) / KM + r[0] * (-2 * gamma + 2 * u1) * (
+                        KM * exp(-(-gamma + u1) ** 2 / (2 * sk)) - x1 - x2 * (
+                            1 + exp(-(B + u1 - u2) ** 2 / (2 * sa)) - exp(-B ** 2 / (2 * sa)))) * exp(
+        (-gamma + u1) ** 2 / (2 * sk)) / (2 * KM * sk)
+    dG2dv = r[1] * (-KM * (-2 * gamma + 2 * u2) * exp(-(-gamma + u2) ** 2 / (2 * sk)) / (2 * sk) + x1 * (
+                2 * B - 2 * u1 + 2 * u2) * exp(-(B - u1 + u2) ** 2 / (2 * sa)) / (2 * sa)) * exp(
+        (-gamma + u2) ** 2 / (2 * sk)) / KM + r[1] * (-2 * gamma + 2 * u2) * (
+                        KM * exp(-(-gamma + u2) ** 2 / (2 * sk)) - x1 * (
+                            1 + exp(-(B - u1 + u2) ** 2 / (2 * sa)) - exp(-B ** 2 / (2 * sa))) - x2) * exp(
+        (-gamma + u2) ** 2 / (2 * sk)) / (2 * KM * sk)
 
     dv1dt = k[0] * dG1dv
     dv2dt = k[1] * dG2dv
@@ -34,30 +59,31 @@ def evoLV(X, t):
     dxvdt = np.array([dx1dt, dx2dt, dv1dt, dv2dt])
     return dxvdt
 
+
 intxv = np.array(IC)
-pop = odeint(evoLV, intxv, range(time+1))
+pop = odeint(evoLV, intxv, range(time + 1))
 
-print ('Equilibrium x1: %f' %pop[time][0])
-print ('Equilibrium u1: %f' %pop[time][2])
+print('Equilibrium x1: %f' % pop[time][0])
+print('Equilibrium u1: %f' % pop[time][2])
 
-print ('Equilibrium x2: %f' %pop[time][1])
-print ('Equilibrium u2: %f' %pop[time][3])
+print('Equilibrium x2: %f' % pop[time][1])
+print('Equilibrium u2: %f' % pop[time][3])
 
-#Obtaining extinction times
-for i in range(len(pop[:,1])):
-    if (pop[:,1][i])<1:
+# Obtaining extinction times
+for i in range(len(pop[:, 1])):
+    if (pop[:, 1][i]) < 1:
         print(i)
         print('Fast')
         extinct_fast = i
         break
 
-for j in range(len(pop[:,0])):
-    if (pop[:,0][j])<1:
+for j in range(len(pop[:, 0])):
+    if (pop[:, 0][j]) < 1:
         print(j)
         print('Slow')
         extinct_slow = j
         break
-        
+
 if strat1 == 0.5:
     txt = 'Close'
 elif strat1 == 4:
@@ -65,22 +91,22 @@ elif strat1 == 4:
 elif strat1 == 10:
     txt = 'Far'
 
-#Code to plot ODE simulation
+# Code to plot ODE simulation
 plt.figure()
 plt.subplot(211)
 plt.title('Sinusoidal Evolutionary Tracking: ' + txt)
-plt.plot(pop[:,0],label='Slow')
-plt.plot(pop[:,1],label='Fast')
-if extinct_fast>1:
-    plt.plot(extinct_fast,pop[:,1][extinct_fast],'kD')
-elif extinct_slow>1:
-    plt.plot(extinct_slow,pop[:,0][extinct_slow],'kD')
+plt.plot(pop[:, 0], label='Slow')
+plt.plot(pop[:, 1], label='Fast')
+if extinct_fast > 1:
+    plt.plot(extinct_fast, pop[:, 1][extinct_fast], 'kD')
+elif extinct_slow > 1:
+    plt.plot(extinct_slow, pop[:, 0][extinct_slow], 'kD')
 plt.legend()
 plt.grid(True)
 plt.ylabel('Pop Size, x')
 plt.subplot(212)
-plt.plot(pop[:,2],label='k = ' + str(k[0]),c='C1')
-plt.plot(pop[:,3],label='k = ' + str(k[1]), c='C0')
+plt.plot(pop[:, 2], label='k = ' + str(k[0]), c='C1')
+plt.plot(pop[:, 3], label='k = ' + str(k[1]), c='C0')
 plt.grid(True)
 plt.ylabel('Indv Strategy, v')
 plt.show()
